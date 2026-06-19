3576f2b9383a6173f962a90a09e74adac74e4c06
Seifeddine <39904276+seif2018@users.noreply.github.com>
2026-06-19 09:01:51 +0200

    Switch API base URL from HTTPS to HTTP

--- frontend/fizzbuzz-ui/src/app/environment.ts
+++ frontend/fizzbuzz-ui/src/app/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiBaseUrl: 'https://localhost:58992'
-  //apiBaseUrl: 'http://localhost:5000'
+  //apiBaseUrl: 'https://localhost:58992'
+  apiBaseUrl: 'http://localhost:5000'
 };
